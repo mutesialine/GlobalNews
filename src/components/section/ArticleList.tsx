427550534f009f05c7e2d44d10c5c5efdf4036ae
mutesialine <ktesialine@gmail.com>
2023-05-16 19:51:45 +0200

    feat: add trending news to the news layouts

--- src/components/section/ArticleList.tsx
+++ src/components/section/ArticleList.tsx
@@ -10,35 +10,61 @@ const ArticleList = () => {
   const swiperRef = useRef<SwiperRef>(null);
 
   return (
-    <div className="w-3/5 px-5 py-8">
-      <div className="relative z-10">
-        <Swiper
-          slidesPerView={1}
-          onSlideChange={() => console.log("slide change")}
-          ref={swiperRef}
-          className="relative"
-        >
-          {newsArticle?.map((article, index) => (
-            <a href="" key={`articles-index${index}`} className="w-full">
-              <SwiperSlide className="relative flex ">
-                <NewsCard {...article} />
-              </SwiperSlide>
-            </a>
-          ))}
-        </Swiper>
-        <div className="absolute right-0 z-20 flex flex-col items-end bottom-16">
-          <div onClick={() => swiperRef.current?.swiper.slideNext()}>
-            <BsChevronRight size={44} className="p-2 bg-red-600" />
-          </div>
-          <div onClick={() => swiperRef.current?.swiper.slidePrev()}>
-            <BsChevronLeft size={44} className="p-2 bg-white" />
+    <div className="flex items-center gap-x-12">
+      <div className="w-3/5 px-5 py-8 space-y-3">
+        <div className="relative z-10">
+          <Swiper
+            slidesPerView={1}
+            onSlideChange={() => console.log("slide change")}
+            ref={swiperRef}
+            className="relative"
+          >
+            {newsArticle?.map((article, index) => (
+              <a href="" key={`articles-index${index}`} className="w-full">
+                <SwiperSlide className="relative flex ">
+                  <NewsCard {...article} />
+                </SwiperSlide>
+              </a>
+            ))}
+          </Swiper>
+          <div className="absolute bottom-0 right-0 z-20 flex flex-col items-end">
+            <div onClick={() => swiperRef.current?.swiper.slideNext()}>
+              <BsChevronRight size={44} className="p-2 bg-red-600" />
+            </div>
+            <div onClick={() => swiperRef.current?.swiper.slidePrev()}>
+              <BsChevronLeft size={44} className="p-2 bg-white" />
+            </div>
           </div>
         </div>
+        <div className="flex gap-x-4">
+          {newsArticle?.slice(3, 10).map((article, index) => (
+            <ImageCard {...article} key={index} />
+          ))}
+        </div>
       </div>
-      <div className="flex gap-x-4">
-        {newsArticle?.slice(3, 10).map((article, index) => (
-          <ImageCard {...article} key={index} />
-        ))}
+      <div>
+        <a href="">trending</a>
+        <div className="flex flex-col gap-y-2">
+          {newsArticle?.slice(5, 9).map((article, index) => (
+            <div
+              className="flex items-center justify-between text-black cursor-pointer gap-x-4 group hover:opacity-90"
+              key={index}
+            >
+              <div className="w-full">
+                <img src={article.urlToImage} alt="image" className="" />
+              </div>
+
+              <div className="">
+                <p className="text-base font-bold gap-x-2 hover:underline">
+                  {article.title}
+                </p>
+                <ul className="pt-2 pl-5 space-y-3 text-sm text-white list-disc marker:text-white">
+                  <li>{article.publishedAt}hours ago</li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
